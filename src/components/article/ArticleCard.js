a814ce906e61c0ea8d1bc2694dfcf0568a395514
Travis Pinson <travispinson@gmail.com>
2020-10-09 14:16:07 -0500

    Fixed article card

--- src/components/article/ArticleCard.js
+++ src/components/article/ArticleCard.js
@@ -4,8 +4,8 @@ export const ArticleCard = ({article}) => (
     <section className="article">
         <h3 className="article__title">{article.title}</h3>
         <div className="article_synopsis">Synopsis: {article.synopsis}</div>
-        <div className="article_author">Author: {article.author}</div>
-        <div className="article_date">Date: {article.date}</div>
+        <div className="article_author">Author: {article.user.name}</div>
+        <div className="article_date">Date: {article.dateAdded}</div>
         <div className="article_url">Url: {article.url}</div>
     </section>
 )
